8cbd8a30493b1a7879f2a32f16b4d3a7025b1d71
Jessica <jessicamenius@gmail.com>
2020-04-18 22:26:33 -0500

    working javascript

--- script.js
+++ script.js
@@ -1,44 +1,8 @@
-generateBtn.addEventListener("click", function(event){
+document.querySelector("button").addEventListener("click", function (event) {
   event.preventDefault();
 
   var length = document.querySelector('input[name="length"]').value;
-  
-    if (length < 8 || length > 128) {
-      alert(
-        "The password needs to be at least 8 characters and less than 128 characters"
-      );
-    } else {
-      var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
-      var specialChars = "~!@#$%^&*()";
-      var numbers = "1234567890";
-  
-      var possibleCharacters = "";
-      var checkboxes = document.querySelectorAll('input[name="types"]:checked');
-  
-      for (var i = 0; i < checkboxes.length; i += 1) {
-        if (checkboxes[i].value === "uppercase") {
-          possibleCharacters += uppercaseCharacters;
-        }
-        if (checkboxes[i].value === "lowercase") {
-          possibleCharacters += lowercaseCharacters;
-        }
-        if (checkboxes[i].value === "special-chars") {
-          possibleCharacters += specialChars;
-        }
-      }
-  
-      var password = "";
-  
-      for (var j = 0; j < length; j += 1) {
-        password += possibleCharacters.charAt(
-          Math.floor(Math.random() * possibleCharacters.length)
-        );
-      }
-  
-      document.getElementById("password").innerHTML = password;
-    }
-  });
+
   if (length < 8 || length > 128) {
     alert(
       "The password needs to be at least 8 characters and less than 128 characters"
@@ -47,12 +11,12 @@ generateBtn.addEventListener("click", function(event){
     var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
     var specialChars = "~!@#$%^&*()";
-    var numbers = "1234567890";
-  
+    var numb1 = "1234567890";
+
     var possibleCharacters = "";
     var checkboxes = document.querySelectorAll('input[name="types"]:checked');
-  
-    for (var i = 0; i < checkboxes.length; i += 1) {
+
+    for (var i = 0; i < checkboxes.length; i++) {
       if (checkboxes[i].value === "uppercase") {
         possibleCharacters += uppercaseCharacters;
       }
@@ -62,19 +26,20 @@ generateBtn.addEventListener("click", function(event){
       if (checkboxes[i].value === "special-chars") {
         possibleCharacters += specialChars;
       }
+      if (checkboxes[i].value === "numb1") {
+        possibleCharacters += numb1;
+      }
     }
-  
+
     var password = "";
-  
-    for (var j = 0; j < length; j += 1) {
+    console.log("password");
+
+    for (var j = 0; j < length; j++) {
       password += possibleCharacters.charAt(
         Math.floor(Math.random() * possibleCharacters.length)
       );
     }
-  
+
     document.getElementById("password").innerHTML = password;
   }
-  });
-
 });
-
